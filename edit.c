693bed3908a1a07aa99c4e7124273e76f18a8895
Michał Czarnecki <czarnecky@va.pl>
2019-03-10 22:02:00 +0100

    Fixed edit_delete(). The string in buf is always NULL-terminated.

--- edit.c
+++ edit.c
@@ -6,7 +6,7 @@ void edit_init(edit *E, char *buf, size_t bufs)
 	E->cur_x = E->cur_y = 0;
 	E->bufs = bufs;
 	E->begin = E->cur = E->end = buf;
-	memset(E->begin, 0, E->bufs);
+	*E->begin = 0;
 }
 
 static void update_cursor_pos(edit *E)
@@ -16,11 +16,11 @@ static void update_cursor_pos(edit *E)
 
 	B = E->begin;
 	while (B != E->cur && (b = utf8_dechar(&cp, B))) {
-		w += utf8_width(cp);
+		w += utf8_cp2w(cp);
 		B += b;
 	}
 	E->cur_x = w;
-	E->cur_y = 0;
+	E->cur_y = 1;
 }
 
 void edit_move(edit *E, int n)
@@ -33,10 +33,9 @@ void edit_move(edit *E, int n)
 	}
 	else if (n < 0) {
 		while (n++ && E->cur != E->begin) {
-			while (!(b = utf8_b2len(E->cur))) {
+			do {
 				E->cur--;
-			}
-			E->cur--;
+			} while (E->cur != E->begin && !(b = utf8_b2len(E->cur)));
 		}
 	}
 	update_cursor_pos(E);
@@ -61,6 +60,7 @@ void edit_insert(edit *E, char *s, size_t sl)
 	memcpy(E->cur, s, sl);
 	E->cur += sl;
 	E->end += sl;
+	*E->end = 0;
 	update_cursor_pos(E);
 }
 
@@ -71,20 +71,22 @@ void edit_delete(edit *E, int n)
 
 	A = B = E->cur;
 	if (n > 0) {
+		if (B == E->end) return;
 		while (n-- && B != E->end && (b = utf8_dechar(&cp, B))) {
 			B += b;
 		}
 	}
 	else if (n < 0) {
+		if (A == E->begin) return;
 		while (n++ && A != E->begin) {
-			while (!(b = utf8_b2len(A))) {
+			do {
 				A--;
-			}
-			A--;
+			} while (A != E->begin && !(b = utf8_b2len(A)));
 		}
 	}
 	memmove(A, B, E->end-B);
 	E->end -= B-A;
 	E->cur = A;
+	*E->end = 0;
 	update_cursor_pos(E);
 }
