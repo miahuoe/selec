c5137b9f29f55e7dda7ca397455d875083703baf
Michał Czarnecki <czarnecky@va.pl>
2019-03-17 16:26:52 +0100

    Added utf8_limit_width(). Implemented proper xgetline(). Using stderr (instead of stdout) as output to allow more flexible usage.

--- intr.c
+++ intr.c
@@ -21,7 +21,6 @@
 
 /* TODO
  * - test & rename spawn()
- * - getline from fd
  * - copy selection on rescan
  * - do pgup/pgdown differently
  */
@@ -44,6 +43,20 @@ void err(const char *fmt, ...)
 	fflush(stderr);
 	fflush(stdout);
 	exit(EXIT_FAILURE);
+}
+
+/* Returns the number of bytes that have maximum width W */
+int utf8_limit_width(char *S, int W)
+{
+	int bytes = 0, b, cp, cpw;
+	while ((b = utf8_dechar(&cp, S)) && W) {
+		cpw = utf8_cp2w(cp);
+		if (W < cpw) break;
+		W -= cpw;
+		bytes += b;
+		S += b;
+	}
+	return bytes;
 }
 
 int spawn(int rw[2], pid_t *p, char **argv[])
@@ -82,23 +95,50 @@ int spawn(int rw[2], pid_t *p, char **argv[])
 	return 0;
 }
 
-int xgetline(int fd, char* buf, size_t bufsize, size_t* linelen)
+/* TODO test other line endings like \r\n */
+int xgetline(int fd, char *buf, size_t bufs, char *b[3])
 {
-	/* TODO */
-	int r;
-	*linelen = 0;
+	char *src, *dst;
+	size_t s;
+	ssize_t r;
+	int i, L = 0;
 
-	(void)bufsize;
-
-	while ((r = read(fd, buf, 1))) {
-		if (*buf == '\n' || *buf == '\r') {
-			*buf = 0;
-			return 0;
-		}
-		++*linelen;
-		buf++;
+	src = b[1];
+	dst = b[0];
+	i = s = b[2] - b[1];
+	while (i--) {
+		*dst++ = *src++;
 	}
-	return 1;
+	b[0] = buf;
+	b[1] = buf;
+	b[2] = buf + s;
+
+	for (;;) {
+		while (*b[1] != '\n' && *b[1] != '\r' && b[2] - b[1] > 0) {
+			b[1]++;
+		}
+		if (b[2] - b[1] > 0 && (*b[1] == '\n' || *b[1] == '\r')) {
+			L = b[1] - b[0];
+			break;
+		}
+		else {
+			r = read(fd, b[2], bufs-(b[2]-b[0]));
+			if (r == -1) {
+				return -2;
+			}
+			if (r == 0) {
+				return -1;
+			}
+			b[2] += r;
+		}
+	}
+	*b[1] = 0;
+	b[1]++;
+	if (b[2] - b[1] > 0 && (*b[1] == '\n' || *b[1] == '\r')) {
+		*b[1] = 0;
+		b[1]++;
+	}
+	return L;
 }
 
 void entry_free(entry *H)
@@ -114,16 +154,14 @@ void entry_free(entry *H)
 int read_entries(int fd, entry **head, entry **last)
 {
 	entry *q;
-	int n = 0, r;
+	int n = 0, L;
 	char buf[BUFSIZ];
-	size_t L = 0;
+	char *b[3] = { buf, buf, buf };
 
 	*head = 0;
-	while (!(r = xgetline(fd, buf, sizeof(buf), &L))) {
-		q = malloc(sizeof(entry)+L+1);
+	while (0 <= (L = xgetline(fd, buf, sizeof(buf), b))) {
+		q = calloc(1, sizeof(entry)+L+1);
 		q->prev = *last;
-		q->next = 0;
-		q->selected = 0;
 		q->L = L;
 		memcpy(q->str, buf, L+1);
 		if (!*head) {
@@ -236,7 +274,7 @@ static void sighandler(int sig)
 	}
 }
 
-static void prepare_window(int *x, int *y)
+static void prepare_window(int fd, int *x, int *y)
 {
 	int i;
 
@@ -247,22 +285,9 @@ static void prepare_window(int *x, int *y)
 	if (g_winh < *y + list_height) {
 		*y = g_winh - (list_height + 1) + 1;
 		for (i = 0; i < list_height; i++) {
-			write(1, "\r\n", 2);
+			write(fd, "\r\n", 2);
 		}
 	}
-}
-
-void fill_line(int linelen, const char *fmt, ...)
-{
-	/* TODO */
-	va_list a;
-	(void)linelen;
-
-	//int w = utf8_strwidth(
-	va_start(a, fmt);
-	dprintf(1, SL(CSI_CLEAR_LINE));
-	vdprintf(1, fmt, a);
-	va_end(a);
 }
 
 static void view_range_make(entry *view_range[2], int list_height, entry *L)
@@ -297,14 +322,41 @@ static void view_range_move(entry *view_range[2], entry **highlight, int n)
 	}
 }
 
+static void draw_view_range(entry *view_range[2], entry *highlight, int W, int H)
+{
+	entry *w;
+	int fd = 2, b;
+
+	w = view_range[0];
+	while (w && H) {
+		if (w == highlight) {
+			dprintf(fd, "\x1b[%c%cm", '3', '0');
+			dprintf(fd, "\x1b[%c%cm", '4', '7');
+		}
+		b = utf8_limit_width(w->str, W-2);
+		dprintf(fd, "%s%c %.*s", CSI_CLEAR_LINE,
+			w == highlight || w->selected ? '>' : ' ', b, w->str);
+		if (w == highlight) {
+			dprintf(fd, "\x1b[%cm", '0');
+		}
+		dprintf(fd, "\r\n");
+		w = w->next;
+		H--;
+	}
+	while (H) {
+		dprintf(fd, "%s \r\n", CSI_CLEAR_LINE);
+		H--;
+	}
+}
+
 int main(int argc, char *argv[])
 {
-	char s[512];
+	char s[512]; /* TODO */
 	char *delim = default_delim,
 	     *subst = default_subst,
-	     *argv0,
-	     **arg[ARG_MAX];
+	     *argv0, **arg[ARG_MAX];
 	int rw[2], wstatus, n = 0, x, y, utflen;
+	int selected = 0;
 	pid_t p[3];
 	_Bool mid = 0, update = 1;
 	struct termios old;
@@ -313,8 +365,6 @@ int main(int argc, char *argv[])
 	entry *H = 0, *L = 0, *highlight = 0;
 	entry *view_range[2] = { 0, 0 };
 	input I;
-
-	edit_init(&E, s, sizeof(s));
 
 	(void)argc;
 
@@ -379,48 +429,32 @@ int main(int argc, char *argv[])
 	raw(&old);
 	get_cur_pos(&x, &y);
 	get_win_dims(&g_winw, &g_winh);
-	prepare_window(&x, &y);
+	prepare_window(2, &x, &y);
+	edit_init(&E, s, sizeof(s));
 
-	//fprintf(stderr, "cur_pos(%d, %d)\r\n", x, y);
-	//fprintf(stderr, "win_dims(%d, %d)\r\n", g_winw, g_winh);
-	//fprintf(stderr, "list_height = %d\r\n", list_height);
 	for (;;) {
-		//fprintf(stderr, "cur_pos(%d, %d)\r\n", x, y);
-		//fprintf(stderr, "edit(%d, %d)\r\n", E.cur_x, E.cur_y);
-
 		set_cur_pos(x, y);
-		entry *w = view_range[0];
-		for (int i = 0; i < list_height; i++) {
-			if (w) {
-				fill_line(g_winw, "%c %s\r\n",
-					w == highlight || w->selected
-					? '>' : ' ', w->str);
-				w = w->next;
-			}
-			else {
-				fill_line(g_winw, "~\r\n");
-			}
-		}
-		fill_line(g_winw, "> %.*s", (int)(E.end-E.begin), E.begin);
-
+		draw_view_range(view_range, highlight, g_winw, list_height);
+		dprintf(2, "%s> %.*s", CSI_CLEAR_LINE, (int)(E.end-E.begin), E.begin);
 		set_cur_pos(1+E.cur_x+2, y+list_height);
-		write(1, SL(CSI_CURSOR_SHOW));
+		write(2, SL(CSI_CURSOR_SHOW));
 		I = get_input();
-		write(1, SL(CSI_CURSOR_HIDE));
+		write(2, SL(CSI_CURSOR_HIDE));
 		set_cur_pos(x, y);
 
 		switch (I.t) {
 		case IT_NONE:
+			//printf("NONE\n");
 		default:
 			break;
 		case IT_UTF8:
 			update = 1;
 			utflen = utf8_b2len(I.utf);
-			fprintf(stderr, "IT_UTF8::%.*s\n", utflen, I.utf);
 			edit_insert(&E, I.utf, utflen);
+			//printf("UTF8::'%.*s'\n", utflen, I.utf);
 			break;
 		case IT_SPEC:
-			fprintf(stderr, "IT_SPEC::%s\n", special_type_str[I.s]);
+			//printf("SPEC::'%s'\n", special_type_str[I.s]);
 			switch (I.s) {
 			default:
 				break;
@@ -461,32 +495,38 @@ int main(int argc, char *argv[])
 			}
 			break;
 		case IT_CTRL:
-			fprintf(stderr, "IT_CTRL::%c\n", I.utf[0]);
+			//printf("CTRL::'%c'\n", I.utf[0]);
 			switch (I.utf[0]) {
 			case 'M':
 			case 'J': /* ENTER */
 				goto end;
 			case 'I': /* TAB */
-				if (highlight) highlight->selected = 1;
+				if (highlight) {
+					highlight->selected = !highlight->selected;
+					selected += highlight->selected ? 1 : -1;
+				}
 				break;
 			}
 			break;
 		}
 
 		if (update) {
-			fprintf(stderr, "update\n");
 			update = 0;
+			selected = 0;
+			highlight = 0;
+			view_range[0] = view_range[1] = 0;
 			entry_free(H);
 			H = L = highlight = 0;
 			if (*E.begin) {
-				fprintf(stderr, "read_entries\n");
 				spawn(rw, p, arg);
 				close(rw[1]); /* TODO */
 
 				read_entries(rw[0], &H, &L);
-				view_range_make(view_range, list_height, H);
-				highlight = view_range[0];
-				wait(&wstatus);
+				if (H) {
+					view_range_make(view_range, list_height, H);
+					highlight = view_range[0];
+					wait(&wstatus);
+				}
 			}
 		}
 
@@ -494,20 +534,21 @@ int main(int argc, char *argv[])
 end:
 	set_cur_pos(x, y);
 	for (int i = 0; i < list_height+1; i++) {
-		fill_line(g_winw, "");
+		dprintf(2, "%s", CSI_CLEAR_LINE);
 		if (i != list_height) {
-			dprintf(1, "\r\n", 2);
+			dprintf(2, "\r\n");
 		}
 	}
 	set_cur_pos(x, y);
 	unraw(&old);
 
-	fprintf(stderr, "'%.*s'\n", (int)(E.end-E.begin), E.begin);
-
+	if (!selected && highlight) {
+		printf("%s\n", highlight->str);
+	}
 	entry *T = H;
 	while (T) {
 		if (T->selected) {
-			fprintf(stderr, "%s\n", T->str);
+			printf("%s\n", T->str);
 		}
 		T = T->next;
 	}
